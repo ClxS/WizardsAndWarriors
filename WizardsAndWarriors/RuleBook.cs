fe0075796194e0dbbcc782bf33ef8c41023bb14b
Carl <slxxls92@gmail.com>
2023-05-22 18:28:12 +0100

    Check for valid equipment

--- WizardsAndWarriors/RuleBook.cs
+++ WizardsAndWarriors/RuleBook.cs
@@ -2,5 +2,5 @@
 
 public static class RuleBook
 {
-    public static bool CanEquip(Player player, Weapon weapon) => true;
+    public static bool CanEquip(Player player, Weapon weapon) => player.ClassSpecification.AllowedWeapons.HasFlag(weapon.Type);
 }
